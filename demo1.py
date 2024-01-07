ce3259898dd7d81c61b5d6d4cd7bbb05ada66e78
Muhammad <bsmuhammad352@gmail.com>
2024-01-07 21:18:18 +0100

    ku2

--- demo1.py
+++ demo1.py
@@ -1,1 +1,15 @@
 import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+kas, frame = cap.read()
+cap.release()
+
+
+img = cv2.imwrite('capImage.png', frame)
+
+img2 = cv2.imread('capImage.png')
+
+cv2.imshow('captured image', img2)
+cv2.waitKey(0)
