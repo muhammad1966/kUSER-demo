2a67d92b0c0fd9929ade3acc3a91c87fc35c8ad7
Muhammad <bsmuhammad352@gmail.com>
2024-01-10 22:35:24 +0100

    ku3

--- demo1.py
+++ demo1.py
@@ -1,4 +1,6 @@
 import cv2
+import face_recognition
+
 
 cap = cv2.VideoCapture(0)
 
@@ -9,7 +11,18 @@ cap.release()
 
 img = cv2.imwrite('capImage.png', frame)
 
+img1 = cv2.imread('muhd.jpg')
 img2 = cv2.imread('capImage.png')
 
-cv2.imshow('captured image', img2)
-cv2.waitKey(0)
+
+image1 = face_recognition.face_encodings(img1)[0]
+image2 = face_recognition.face_encodings(img2)[0]
+
+result = face_recognition.compare_faces([image1], image2)
+
+if result:
+    print(True)
+else:
+    print(False)
+
+
