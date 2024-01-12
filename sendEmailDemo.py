8d8149cb22648dfd22db0293d6f698897f9bc11b
Muhammad <bsmuhammad352@gmail.com>
2024-01-12 21:55:40 +0100

    ku11

--- sendEmailDemo.py
+++ sendEmailDemo.py
@@ -1,27 +1,29 @@
 import smtplib
 import getpass
 
-HOST = 'smtp.gmail.com'
+HOST = 'smtp-mail.outlook.com'
 PORT = 587
 
-from_email = 'muhammadsanusi909@gmail.com'
+from_email = 'muhammadsunusi909@outlook.com'
 to_email = 'bsmuhammad352@gmail.com'
 password = getpass.getpass('Enter your password: ')
 
-message = '''Subject: Demo email
-Hey, this is just a demo email
-'''
+message = f'Subject: Demo email\r\n\r\nHey, this is just a demo email\r\n'
 
-smtp = smtplib.SMTP(HOST, PORT)
+try:
+    smtp = smtplib.SMTP(HOST, PORT)
 
-status_code, response = smtp.ehlo()
-print (f'[*] Pinging the server: {status_code} {response}')
+    status_code, response = smtp.ehlo()
+    print(f'[*] Pinging the server: {status_code} {response}')
 
-status_code, response = smtp.starttls()
-print (f'[*] Starting tls connection: {status_code} {response}')
+    status_code, response = smtp.starttls()
+    print(f'[*] Starting tls connection: {status_code} {response}')
 
-status_code, response = smtp.login(from_email, password)
-print (f'[*] Loging into the server: {status_code} {response}')
+    status_code, response = smtp.login(from_email, password)
+    print(f'[*] Loging into the server: {status_code} {response}')
 
-smtp.sendmail(from_email, to_email, message)
-smtp.quit()
+    smtp.sendmail(from_email, to_email, message)
+    smtp.quit()
+    print('Sent')
+except Exception as e:
+    print(f"An error occurred: {e}")
