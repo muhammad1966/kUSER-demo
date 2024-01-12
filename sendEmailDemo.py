1e956181c6be250d56cb7b175f94c000b175e73c
Muhammad <bsmuhammad352@gmail.com>
2024-01-12 20:54:30 +0100

    ku10

--- sendEmailDemo.py
+++ sendEmailDemo.py
@@ -15,10 +15,10 @@ Hey, this is just a demo email
 smtp = smtplib.SMTP(HOST, PORT)
 
 status_code, response = smtp.ehlo()
-print (f'[*] Echoing the server: {status_code} {response}')
+print (f'[*] Pinging the server: {status_code} {response}')
 
 status_code, response = smtp.starttls()
-print (f'[*] starting tls: {status_code} {response}')
+print (f'[*] Starting tls connection: {status_code} {response}')
 
 status_code, response = smtp.login(from_email, password)
 print (f'[*] Loging into the server: {status_code} {response}')
